9e2acd48f08dab15882ffb9254fa88f39185d42c
wishyoulikebefore <37919975+wishyoulikebefore@users.noreply.github.com>
2018-09-13 17:12:48 +0800

    Update RNAseq_volcano.py

--- seaborn_practice/RNAseq_volcano.py
+++ seaborn_practice/RNAseq_volcano.py
@@ -9,7 +9,7 @@ import seaborn as sns
 def parse_args():
     parser = argparse.ArgumentParser(description="绘制RNAseq的火山图")
     parser.add_argument("-f")
-    parser.add_argument("-n",required=False,default=10)   #点亮上调或下调前多少的基因
+    parser.add_argument("-n",type=int,required=False,default=10)  # 点亮上调或下调前多少的基因
     parser.add_argument("-p",required=False,default=0.05 )
     parser.add_argument("-fc",required=False,default=2)
     args = parser.parse_args()
@@ -19,7 +19,7 @@ def add_text(ax,df,y_axis):
     if len(df) > 0:
         ax.text(1.02, y_axis, "Top %s Up-regulated Genes" %(len(df)), fontsize=15, transform=ax.transAxes)
         for n,(x,y,gene) in enumerate(zip(df["log2FC"],df["padj2"],df.index)):
-            ax.text(x, y, n+1, ha='center', va='center', fontsize=10)
+            ax.text(x, y, n+1, ha='center', va='center', fontsize=8)
             ax.text(1.02, y_axis-0.04*(n+1), "%s  %s" %(n+1,gene), fontsize=10, transform=ax.transAxes)
     else:
         return
@@ -31,12 +31,12 @@ def draw_volcano(csv_file,padj,fc,num):
     expression_matrix.loc[(expression_matrix["sig"] == True) & (expression_matrix["log2FC"] > 0), "type"] = "up"
     expression_matrix.loc[(expression_matrix["sig"] == True) & (expression_matrix["log2FC"] < 0), "type"] = "down"
     expression_matrix.loc[(expression_matrix["sig"] == False), "type"] = "normal"
-    expression_matrix["size"] = np.where(expression_matrix["sig"],6,3)
-    up_df_10 = expression_matrix[expression_matrix["type"] == "up"].sort_values(by="log2FC",ascending=False)[:10]
-    down_df_10 = expression_matrix[expression_matrix["type"] == "down"].sort_values(by="log2FC",ascending=True)[:10]
-    xlim = max(max(up_df_10["log2FC"].abs()),max(down_df_10["log2FC"].abs()))*1.1
+    expression_matrix["size"] = np.where(expression_matrix["sig"],8,4)
+    up_df_ = expression_matrix[expression_matrix["type"] == "up"].sort_values(by="log2FC",ascending=False)[:num]
+    down_df_ = expression_matrix[expression_matrix["type"] == "down"].sort_values(by="log2FC",ascending=True)[:num]
+    xlim = max(max(up_df_["log2FC"].abs()),max(down_df_["log2FC"].abs()))*1.1
     ylim = max(expression_matrix["padj2"])*1.1
-    cmap = ["lightgrey","salmon","mediumpurple"]
+    cmap = {"normal":"lightgrey","up":"red","down":"mediumpurple"}     #擦，原来palette接受dict类型
     sns.scatterplot(x="log2FC", y="padj2", hue="type", size="size", palette=cmap, legend=False, data=expression_matrix)
     ax.set_xlim([-xlim,xlim])
     ax.set_ylim([0,ylim])
@@ -48,9 +48,9 @@ def draw_volcano(csv_file,padj,fc,num):
     ax.set_xlabel("log2FC")
     ax.set_ylabel("-log10(padj)")
     ax.set_title("volcano plot")
-    add_text(ax,up_df_10,1)
-    add_text(ax,down_df_10,0.5)
-    f.savefig('volcano.png', dpi=100, bbox_inches='tight')
+    add_text(ax,up_df_,1)
+    add_text(ax,down_df_,0.5)
+    f.savefig('volcano.pdf', dpi=100, bbox_inches='tight')
 
 if __name__ == "__main__":
     csv_file, padj, fc, num = parse_args()
